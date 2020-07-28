d8d29234a571d5933bcef4b9c184060ef9bb119a
James <jameslukebartlett@gmail.com>
2020-07-28 17:43:40 +0100

    Added more code and integrated the template

--- cvbuilder/tests.py
+++ cvbuilder/tests.py
@@ -1,7 +1,17 @@
 from django.test import TestCase
+from django.urls import resolve
+from cvbuilder.views import home_page
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
-# Create your tests here.
+class HomePageTest(TestCase):
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 2)
+    def test_root_url_resolves_to_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+
+        response = self.client.get('/')
+
+        self.assertTemplateUsed(response, 'template.html')
