b3e09ceb0281d4a3791d72dd4e534e12445bbdb0
James <jameslukebartlett@gmail.com>
2020-08-05 18:11:26 +0100

    WOrked and solved th issue

--- cvbuilder/views.py
+++ cvbuilder/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from cvbuilder.models import SkillItem
+from cvbuilder.models import SkillItem, AchItem
 
 # Create your views here.
 def home_page(request):
     if request.method == 'POST' :
-        SkillItem.objects.create(text=request.POST['skill'])
-        return redirect('/')
+        if request.POST.get('skill', ''):
+            SkillItem.objects.create(text=request.POST['skill'])
+            return redirect('/')
+
+        elif request.POST.get('achievement', '') :
+            AchItem.objects.create(text=request.POST['achievement'])
+            return redirect('/')
 
     skillitems = SkillItem.objects.all()
-    return render(request, 'template.html', {'skillitems': skillitems})
+    achitems = AchItem.objects.all()
+
+    return render(request, 'template.html', {'skillitems': skillitems, 'achitems': achitems})
