49c76cd17c7e84a460e356106aee1bb6c31163b9
James <jameslukebartlett@gmail.com>
2020-08-11 15:37:52 +0100

    Made some alterations and added more fields

--- cvbuilder/models.py
+++ cvbuilder/models.py
@@ -10,6 +10,13 @@ class AchItem(models.Model) :
 class NameItem(models.Model) :
     text = models.TextField(default='')
 
+class NumberItem(models.Model) :
+    text = models.TextField(default='')
+class EmailItem(models.Model) :
+    text = models.TextField(default='')
+class PersonalProfileItem(models.Model) :
+    text = models.TextField(default='')
+
 class WorkExperience(models.Model) :
     company = models.TextField(default='')
     role = models.TextField(default = '')
@@ -22,3 +29,13 @@ class School(models.Model) :
     startdate = models.TextField(default='')
     enddate = models.TextField(default='')
     qualifications = models.TextField(default = '')
+
+class Qualifications(models.Model) :
+    text = models.TextField(default='')
+
+class Projects(models.Model):
+    title = models.TextField(default='')
+    description=models.TextField(default='')
+
+class Notes(models.Model) :
+    note=models.TextField(default='')
--- cvbuilder/views.py
+++ cvbuilder/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from cvbuilder.models import SkillItem, AchItem, WorkExperience, School
+from cvbuilder.models import *
 from cvbuilder.forms import WorkExperienceForm, SchoolForm
 
 # Create your views here.
@@ -19,17 +19,46 @@ def home_page(request):
         elif request.POST.get('EducationSubmit', '') :
             School.objects.create(school= request.POST['school'], startdate=request.POST['startdates'], enddate=request.POST['enddates'], qualifications=request.POST['grades'])
             return redirect('/')
+        elif request.POST.get('name', '') :
+            NameItem.objects.create(text=request.POST['name'])
+            return redirect('/')
+        elif request.POST.get('email', '') :
+            EmailItem.objects.create(text=request.POST['email'])
+            return redirect('/')
+        elif request.POST.get('number', '') :
+            NumberItem.objects.create(text=request.POST['number'])
+            return redirect('/')
+        elif request.POST.get('personalprof', '') :
+            PersonalProfileItem.objects.create(text=request.POST['personalprof'])
+            return redirect('/')
+        elif request.POST.get('qualification', '') :
+            Qualifications.objects.create(text=request.POST['qualification'])
+            return redirect('/')
+        elif request.POST.get('ProjectSubmit', '') :
+            Projects.objects.create(title=request.POST['projectover'], description=request.POST['Projectdes'])
+            return redirect('/')
+        elif request.POST.get('Notes','') :
+            Notes.objects.create(note=request.POST['Notes'])
+            return redirect('/')
+
 
 
     skillitems = SkillItem.objects.all()
     achitems = AchItem.objects.all()
-    nameitems = request.POST.get('name', '')
-    emailitems = request.POST.get('email', '')
-    numberitems = request.POST.get('number', '')
-    personalitems = request.POST.get('personalprof', '')
+    nameitems = NameItem.objects.all().last()
+    emailitems = EmailItem.objects.all().last()
+    numberitems = NumberItem.objects.all().last()
+    personalitems = PersonalProfileItem.objects.all().last()
     workitems= WorkExperience.objects.all()
     schoolitems=School.objects.all()
+    qualificationitems = Qualifications.objects.all()
+    projectitems= Projects.objects.all()
+    noteitems = Notes.objects.all()
 
-    return render(request, 'template.html', {'skillitems': skillitems, 'achitems': achitems, 'name_item': nameitems,
-    'email_item' : emailitems, 'number_item': numberitems, 'profile_item':personalitems,
-    'workitems': workitems, 'schoolitems':schoolitems})
+    return render(request, 'template.html', {'skillitems': skillitems, 'achitems': achitems,
+    'nameitems': nameitems,'emailitems' : emailitems,
+    'numberitems': numberitems, 'personalitems':personalitems,
+    'workitems': workitems, 'schoolitems':schoolitems,
+    'qualificationitems':qualificationitems, 'projectitems' : projectitems,
+    'noteitems' : noteitems,
+    })
